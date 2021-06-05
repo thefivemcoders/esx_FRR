6990d1d034b74a2aa5e96fd763093157e8947424
TheFiveMCoders <44032346+thefivemcoders@users.noreply.github.com>
2021-06-05 16:58:00 +0530

    Create en.lua

---
+++ locales/en.lua
@@ -0,0 +1,24 @@
+-- Coded By The Fivem Coders (https://www.fivemcoders.tech)
+Locales['en'] = {
+
+	['robbery_cancelled'] = 'the Attack will be cancelled, you will gain nothing',
+	['robbery_successful'] = 'successful Attack you have paid ~g~$.',
+	['bank_robbery'] = 'Fort Zancudo Attack',
+	['press_to_rob'] = 'Press ~INPUT_CONTEXT~ to rob ~b~',
+	['robbery_of'] = 'Attacking Fort Zancudo: ~r~',
+	['seconds_remaining'] = '~w~ seconds',
+	['robbery_cancelled_at'] = '~r~ Fort Zancudo Attack cancelled at: ~b~',
+	['robbery_has_cancelled'] = '~r~ Fort Zancudo Attack has been cancelled: ~b~',
+	['already_robbed'] = 'this Place has already been Attacked. Please wait',
+	['seconds'] = 'seconds.',
+	['rob_in_prog'] = '~r~ Attack in progress: ~b~',
+	['started_to_rob'] = 'you started Attack ',
+	['do_not_move'] = ', Do not move away!',
+	['alarm_triggered'] = 'the alarm has been triggered',
+	['hold_pos'] = 'hold for 10 minutes and the money is yours!',
+	['robbery_complete'] = '~r~ Attack complete.~s~ ~h~ Run!',
+	['robbery_complete_at'] = '~r~ Attack complete at: ~b~',
+	['min_two_police'] = 'Number of policemen needed: ',
+	['robbery_already'] = '~r~An Attack is already in progress.',
+
+}
